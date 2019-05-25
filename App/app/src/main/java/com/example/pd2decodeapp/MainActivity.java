2b01099478a3b14d37fa000dbabbc08db312b37d
JQueimado <jpqueimado@gmail.com>
2019-05-25 16:44:05 +0100

    Create_New_Project

---
+++ App/app/src/main/java/com/example/pd2decodeapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pd2decodeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
